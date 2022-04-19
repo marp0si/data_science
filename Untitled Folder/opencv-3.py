486a38428425386d34e7650d2a5c0fc20a86c2a7
marp0si <51880476+marp0si@users.noreply.github.com>
2022-04-20 02:51:06 +0300

    .

---
+++ Untitled Folder/opencv-3.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+import imageio
+
+face_cascade=cv2.CascadeClassifier("haarcascade-frontalface-default.xml")
+
+def detect(frame):
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces=face_cascade.detectMultiScale(gray,1.3,5)
+    for (x,y,w,h) in faces:
+        cv2.regtangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray=gray[y:y+h,x:x+w]
+        roi_color=frame[y:y+h,x:x+w]
+    return
+
+image=imageio.imread("face.jpeg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image=detect(frame=image)
+imageio.imwrite("output.jpg",image)
--- Untitled Folder/opencv.py
+++ Untitled Folder/opencv.py
@@ -64,3 +64,55 @@ cv2.imshow("blured",blured)
 cv2.waitKey()
 cv2.destroyAllWindows()
 '''
+'''
+#cv2.rectangle(resim,başlangıçN,bitişN,renk,kalınlık)
+img=cv2.imread("thumb-1920-1183122.jpg",1)
+cv2.imshow("girdi",img)
+başlangıçN=(15,15)
+bitişN=(250,350)
+renk=(0,0,255)
+kalınlık=2
+dst=cv2.rectangle(img,başlangıçN,bitişN,renk,kalınlık)
+cv2.imshow("çıktı",dst)
+cv2.waitKey()
+cv2.destroyAllWindows()
+'''
+'''
+#cv2.circle(resim,merkez,yarıçap,renk,kalınlık)
+img=cv2.imread("thumb-1920-1183122.jpg",1)
+cv2.imshow("girdi",img)
+merkez=(100,100)
+yarıçap=50
+renk=(0,0,255)
+kalınlık=2
+dst=cv2.circle(img,merkez,yarıçap,renk,kalınlık)
+cv2.imshow("çıktı",dst)
+cv2.waitKey()
+cv2.destroyAllWindows()
+'''
+'''
+#cv2.line(resim,başlangıçN,bitişN,renk,kalınlık)
+img=cv2.imread("thumb-1920-1183122.jpg",1)
+başlangıçN=(15,15)
+bitişN=(250,350)
+renk=(0,0,255)
+kalınlık=2
+dst=cv2.line(img,başlangıçN,bitişN,renk,kalınlık)
+cv2.imshow("çıktı",dst)
+cv2.waitKey()
+cv2.destroyAllWindows()
+'''
+'''
+#cv2.putText(image, text, org, font, fontScale, color[, thickness[, lineType[, bottomLeftOrigin]]])
+img=cv2.imread("thumb-1920-1183122.jpg",1)
+text="bunu yazan tosun okuyana kosun"
+font = cv2.FONT_HERSHEY_SIMPLEX
+fontScale = 1
+org = (50, 50)
+color = (255, 0, 0)
+thickness = 2
+image = cv2.putText(img, text, org, font, fontScale, color, thickness, cv2.LINE_AA,False)
+cv2.imshow("yazılı resim", img) 
+cv2.waitKey()
+cv2.destroyAllWindows()
+'''
